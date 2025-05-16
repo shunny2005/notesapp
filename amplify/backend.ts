c16a4fb37eb0a2ecf3e7c9c41834b2a245786fc3
shunny2005 <shangov963@gmail.com>
2025-05-15 22:16:29 -0700

    Updated backend.ts, removed resource.ts, added new datas folder

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { datas } from './datas/resource';
 import { storage } from './storage/resource';
 
 /**
